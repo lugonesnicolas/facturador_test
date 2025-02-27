28030f5aea221c1319863b93f4f76e22d8c82da2
Nicolas  Ezequiel Lugones <82919404+lugonesnicolas@users.noreply.github.com>
2025-02-27 00:32:44 -0300

    Resolucion any en product

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,6 +1,12 @@
 import ProductCard from "@/components/ProductCard";
 
-async function getProducts() {
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+}
+
+async function getProducts(): Promise<Product[]> {
   const res = await fetch("https://fakestoreapi.com/products");
   if (!res.ok) throw new Error("Error al obtener los productos");
   return res.json();
@@ -13,7 +19,7 @@ export default async function ProductsPage() {
     <main className="container mx-auto p-6">
       <h1 className="text-3xl font-bold text-gray-800 mb-6">Lista de Productos</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {products.map((product: any) => (
+        {products.map((product: Product) => (
           <ProductCard
             key={product.id}
             id={product.id.toString()}
